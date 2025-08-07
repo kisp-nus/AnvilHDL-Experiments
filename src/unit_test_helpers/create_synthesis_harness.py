69905b9d7c62f1891c0884fbf8e0d1356128fe5f
adi4comp <adi4workspace@gmail.com>
2025-08-07 19:35:39 +0800

    fix synth harness generation

--- src/unit_test_helpers/create_synthesis_harness.py
+++ src/unit_test_helpers/create_synthesis_harness.py
@@ -5,6 +5,19 @@ import os
 CUR_DIR = os.path.dirname(os.path.abspath(__file__))
 
 
+def get_all_files_in_dir(directory):
+    if not os.path.exists(directory):
+        print(f"Error: Directory {directory} does not exist.")
+        sys.exit(1)
+        
+    all_files = []
+    for root, _, files in os.walk(directory):
+        for file in files:
+            if file.endswith(('.sv', '.svh')):
+                all_files.append(os.path.abspath(os.path.join(root, file)))
+    return all_files
+
+
 def get_testbench_dependencies(RTL_DIR, flist):
     if not os.path.exists(RTL_DIR):
         print(f"Error: RTL directory {RTL_DIR} does not exist.")
@@ -17,12 +30,28 @@ def get_testbench_dependencies(RTL_DIR, flist):
         
     with open(flist_path, 'r') as file:
         dependencies_list = file.readlines()
-        cleaned_dependencies = [line.replace("+incdir+", "").strip() for line in dependencies_list]
-        extended_dependencies = [os.path.join(RTL_DIR, dep) for dep in os.listdir(RTL_DIR) if dep.endswith('.sv')] + cleaned_dependencies
-        return extended_dependencies
-    
+        
+        inc_dirs = [line.split("+incdir+")[1].strip() for line in dependencies_list if line.startswith("+incdir+")]
+        print(f"Found {len(inc_dirs)} include directories: {inc_dirs}")
+
+        for dir in inc_dirs:
+            all_files = get_all_files_in_dir(dir)
+            dependencies_list.extend(all_files)
+
+        dependencies_list += get_all_files_in_dir(RTL_DIR)
+        cleaned_dependencies = [dep.strip() for dep in dependencies_list if dep.strip() and not dep.startswith("+incdir+")]
+        return cleaned_dependencies
+        
 
 
+def preprocess_files(file):
+    if not os.path.exists(file):
+        print(f"Error: File {file} does not exist.")
+        sys.exit(1)
+    with open(file, 'r') as f:
+        content = f.read()
+        content = content.replace('`include', '// `include')
+        return content
 
 def create_synthesis_harness(dependencies_list):
     SYNTH_DIR = os.path.join(CUR_DIR, "synthesis")
@@ -30,10 +59,10 @@ def create_synthesis_harness(dependencies_list):
     os.makedirs(os.path.join(SYNTH_DIR, "rtl"), exist_ok=True)
     print(f"Synthesis directory created at: {SYNTH_DIR}")
     for dep in dependencies_list:
-        os.system(f"cp {dep} {SYNTH_DIR}/rtl/")
-
-    
-    os.system(f"zip -r synthesis_harness.zip {SYNTH_DIR}")
+        content = preprocess_files(dep)
+        with open(f"{SYNTH_DIR}/rtl/{os.path.basename(dep)}", 'w') as f:
+            f.write(content)
+    os.system(f"zip -r synthesis_harness.zip synthesis")
     os.system(f"rm -rf {SYNTH_DIR}")
 
 def main():
